a62b23c14c042fa283a274265f2c9e431259b1a1
桔梗迷 <ikikyou6@qq.com>
2023-04-19 17:44:19 +0800

    使用若依

---
+++ portal/src/api/login.js
@@ -0,0 +1,61 @@
+import request from '@/utils/request'
+import qs from 'qs'
+
+// 登录方法
+export function login(username, password, code, uuid) {
+  const data = {
+    username,
+    password,
+    code,
+    uuid
+  }
+  return request({
+    url: '/login',
+    headers: {
+      isToken: false,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    method: 'post',
+    data: qs.stringify(data)
+  })
+}
+
+// 注册方法
+export function register(data) {
+  return request({
+    url: '/register',
+    headers: {
+      isToken: false
+    },
+    method: 'post',
+    data: data
+  })
+}
+
+// 获取用户详细信息
+export function getInfo() {
+  return request({
+    url: '/user/info',
+    method: 'get'
+  })
+}
+
+// 退出方法
+export function logout() {
+  return request({
+    url: '/logout',
+    method: 'post'
+  })
+}
+
+// 获取验证码
+export function getCodeImg() {
+  return request({
+    url: '/captchaImage',
+    headers: {
+      isToken: false
+    },
+    method: 'get',
+    timeout: 20000
+  })
+}
